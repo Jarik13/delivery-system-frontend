d1d1a7d203387494eaf7196b678c8b7fd54720af
Yaroslav Guz <aroslavguz@gmail.com>
2026-02-24 01:51:12 +0200

    Add new header to api

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -3,7 +3,7 @@ import qs from 'qs';
 
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL || 'http://localhost:4000/api/v1',
-    headers: { 'Content-Type': 'application/json' },
+    headers: { 'Content-Type': 'application/json', 'ngrok-skip-browser-warning': 'true' },
     paramsSerializer: params => qs.stringify(params, { arrayFormat: 'repeat' })
 });
 
