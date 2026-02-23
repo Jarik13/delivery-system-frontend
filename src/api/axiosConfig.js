38cc0b99608ab08124b8b1b1978442d9fe944530
Yaroslav Guz <aroslavguz@gmail.com>
2026-02-24 01:26:41 +0200

    Add env backend url

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import qs from 'qs';
 
 const api = axios.create({
-    baseURL: 'http://localhost:4000/api/v1',
+    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:4000/api/v1',
     headers: { 'Content-Type': 'application/json' },
     paramsSerializer: params => qs.stringify(params, { arrayFormat: 'repeat' })
 });
