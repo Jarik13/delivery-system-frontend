5ff39e299d8391f4dd967ce5e9f1ee39a160e899
Yaroslav Guz <aroslavguz@gmail.com>
2026-04-04 12:23:12 +0300

    Add displaying destanationAddress

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.q5p14tuujdg"
+    "revision": "0.mu3mc89o9eg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
