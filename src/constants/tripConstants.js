042ee663567b8d4123b6e9a942669a29f13241c1
Yaroslav Guz <aroslavguz@gmail.com>
2026-04-10 18:07:36 +0300

    Block trip delete and edit functional if it status 'У дорозі'

--- src/constants/tripConstants.js
+++ src/constants/tripConstants.js
@@ -1,2 +1,2 @@
-export const TRIP_EDITABLE_STATUSES = ['Заплановано', 'Завантаження', 'В дорозі', 'Розвантаження'];
+export const TRIP_EDITABLE_STATUSES = ['Заплановано', 'Завантаження', 'У дорозі', 'Розвантаження'];
 export const isTripEditable = (statusName) => TRIP_EDITABLE_STATUSES.includes(statusName);
